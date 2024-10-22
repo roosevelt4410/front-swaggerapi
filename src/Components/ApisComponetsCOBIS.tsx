a28fa6939a6abeebcdf565c9cad7656e2d0a28f6
andres_4410 <andres_riobo@gtccorporation.com>
2024-10-22 09:33:22 -0500

    Update deploy archivos de configuracion customer

--- src/Components/ApisComponetsCOBIS.tsx
+++ src/Components/ApisComponetsCOBIS.tsx
@@ -16,73 +16,75 @@ const ApisComponetsCOBIS: React.FC = () => {
   const [selectedApi, setSelectedApi] = useState<string | null>(null);
   const [openCategory, setOpenCategory] = useState<string | null>(null);
   const [activeApi, setActiveApi] = useState<string | null>(null);
+  /* const backendUrl = "http://localhost:3000"; */
+  const backendUrl = "https://main.d10lb53q0mw9au.amplifyapp.com";
 
   const apis: ApiCategories = {
     Loans:[
-      { name: "Loans API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISLoansAPI-resolved-v2-0-0-min.json` },
-      { name: "Collateral API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCollateralAPI-resolved-v1-0-11-min.json` },
-      { name: "Collateral Policies API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCollateralPoliciesAPI-resolved-v1-0-0-min.json` },
-      { name: "Collaterals Association API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCollateralsAssociationAPI-resolved-v1-0-7-min.json` },
-      { name: "Collaterals Attributes API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCollateralsAttributesAPI-resolved-v1-0-1-min.json` },
-      { name: "Credit Line Additional Data API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCreditLineAdditionalDataAPI-resolved-v1-0-1-min.json` },
-      { name: "Credit Line API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCreditLineAPI-resolved-v1-0-1-min.json` },
-      { name: "Credit Line Collaterals API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCreditLineCollateralsAPI-resolved-v1-0-1-min.json` },
-      { name: "Credit Line Commissions API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCreditLineCommissionsAPI-resolved-v1-0-1-min.json` },
-      { name: "Credit Line Distribution API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISCreditLineDistributionAPI-resolved-v1-0-1-min.json` },
-      { name: "Debtors Loans API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISDebtorsLoansAPI-resolved-v1-0-2-min.json` },
-      { name: "Disbursements Loans API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISDisbursementsLoansAPI-resolved-v1-1-7-min.json` },
-      { name: "Insurance API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISInsuranceAPI-resolved-v1-0-1-min.json` },
-      { name: "Loans Amortizations API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISLoansAmortizationsAPI-resolved-v1-0-0-min.json` },
-      { name: "Loans Balances API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISLoansBalancesAPI-resolved-v1-0-0-min.json` },
-      { name: "Loans Change Situation API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISLoansChangeSituationAPI-resolved-v1-0-0-min.json` },
-      { name: "Loans Operations API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISLoansOperationsAPI-resolved-v1-0-6-min.json` },
-      { name: "Loans Simulations API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISLoansSimulationsAPI-resolved-v1-0-0-min.json` },
-      { name: "Payments Loans API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISPaymentsLoansAPI-resolved-v1-1-8-min.json` },
-      { name: "Subsidy Loss API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISSubsidyLossAPI-resolved-v1-0-1-min.json` },
-      { name: "Transactional Loans API", url: `${process.env.PUBLIC_URL}/LOANSCOBIS/COBISTransactionalLoansAPI-resolved-v2-0-0-min.json` }
+      { name: "Loans API", url: `${backendUrl}/LOANSCOBIS/COBISLoansAPI-resolved-v2-0-0-min.json` },
+      { name: "Collateral API", url: `${backendUrl}/LOANSCOBIS/COBISCollateralAPI-resolved-v1-0-11-min.json` },
+      { name: "Collateral Policies API", url: `${backendUrl}/LOANSCOBIS/COBISCollateralPoliciesAPI-resolved-v1-0-0-min.json` },
+      { name: "Collaterals Association API", url: `${backendUrl}/LOANSCOBIS/COBISCollateralsAssociationAPI-resolved-v1-0-7-min.json` },
+      { name: "Collaterals Attributes API", url: `${backendUrl}/LOANSCOBIS/COBISCollateralsAttributesAPI-resolved-v1-0-1-min.json` },
+      { name: "Credit Line Additional Data API", url: `${backendUrl}/LOANSCOBIS/COBISCreditLineAdditionalDataAPI-resolved-v1-0-1-min.json` },
+      { name: "Credit Line API", url: `${backendUrl}/LOANSCOBIS/COBISCreditLineAPI-resolved-v1-0-1-min.json` },
+      { name: "Credit Line Collaterals API", url: `${backendUrl}/LOANSCOBIS/COBISCreditLineCollateralsAPI-resolved-v1-0-1-min.json` },
+      { name: "Credit Line Commissions API", url: `${backendUrl}/LOANSCOBIS/COBISCreditLineCommissionsAPI-resolved-v1-0-1-min.json` },
+      { name: "Credit Line Distribution API", url: `${backendUrl}/LOANSCOBIS/COBISCreditLineDistributionAPI-resolved-v1-0-1-min.json` },
+      { name: "Debtors Loans API", url: `${backendUrl}/LOANSCOBIS/COBISDebtorsLoansAPI-resolved-v1-0-2-min.json` },
+      { name: "Disbursements Loans API", url: `${backendUrl}/LOANSCOBIS/COBISDisbursementsLoansAPI-resolved-v1-1-7-min.json` },
+      { name: "Insurance API", url: `${backendUrl}/LOANSCOBIS/COBISInsuranceAPI-resolved-v1-0-1-min.json` },
+      { name: "Loans Amortizations API", url: `${backendUrl}/LOANSCOBIS/COBISLoansAmortizationsAPI-resolved-v1-0-0-min.json` },
+      { name: "Loans Balances API", url: `${backendUrl}/LOANSCOBIS/COBISLoansBalancesAPI-resolved-v1-0-0-min.json` },
+      { name: "Loans Change Situation API", url: `${backendUrl}/LOANSCOBIS/COBISLoansChangeSituationAPI-resolved-v1-0-0-min.json` },
+      { name: "Loans Operations API", url: `${backendUrl}/LOANSCOBIS/COBISLoansOperationsAPI-resolved-v1-0-6-min.json` },
+      { name: "Loans Simulations API", url: `${backendUrl}/LOANSCOBIS/COBISLoansSimulationsAPI-resolved-v1-0-0-min.json` },
+      { name: "Payments Loans API", url: `${backendUrl}/LOANSCOBIS/COBISPaymentsLoansAPI-resolved-v1-1-8-min.json` },
+      { name: "Subsidy Loss API", url: `${backendUrl}/LOANSCOBIS/COBISSubsidyLossAPI-resolved-v1-0-1-min.json` },
+      { name: "Transactional Loans API", url: `${backendUrl}/LOANSCOBIS/COBISTransactionalLoansAPI-resolved-v2-0-0-min.json` }
     ],
     Customers:[
-      { name: "Additional Information Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISAdditionalInformationCustomersAPI.json` },
-      { name: "Businesses Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISBusinessesCustomersAPI.json` },
-      { name: "Contacts Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISContactsCustomersAPI.json` },
-      { name: "Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISCustomersAPI.json` },
-      { name: "Customers Balance API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISCustomersBalanceAPI.json` },
-      { name: "Economic Groups Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISEconomicGroupsCustomersAPI.json` },
-      { name: "Foreign Accounts API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISForeignAccountsAPI.json` },
-      { name: "Legal Persons Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISLegalPersonsCustomersAPI.json` },
-      { name: "Products Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISProductsCustomersAPI.json` },
-      { name: "References Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISReferencesCustomersAPI.json` },
-      { name: "Relationships Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISRelationshipsCustomersAPI.json` },
-      { name: "Solidarity Groups Customers API", url: `${process.env.PUBLIC_URL}/CUSTOMERCOBIS/COBISSolidarityGroupsCustomersAPI.json` }
+      { name: "Additional Information Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISAdditionalInformationCustomersAPI-resolved-v1-1-6.json` },
+      { name: "Businesses Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISBusinessesCustomersAPI-resolved-v1-3-5.json` },
+      { name: "Contacts Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISContactsCustomersAPI-resolved-v4-0-5.json` },
+      { name: "Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISCustomersAPI-resolved-v3-1-7.json` },
+      { name: "Customers Balance API", url: `${backendUrl}/CUSTOMERCOBIS/COBISCustomersBalanceAPI-resolved-v1-4-0.json` },
+      { name: "Economic Groups Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISEconomicGroupsCustomersAPI-resolved-v1-1-1.json` },
+      { name: "Foreign Accounts API", url: `${backendUrl}/CUSTOMERCOBIS/COBISForeignAccountsAPI-resolved-v1-1-0.json` },
+      { name: "Legal Persons Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISLegalPersonsCustomersAPI-resolved-v1-4-6.json` },
+      { name: "Products Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISProductsCustomersAPI-resolved-v1-0-3.json` },
+      { name: "References Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISReferencesCustomersAPI-resolved-v1-1-1.json` },
+      { name: "Relationships Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISRelationshipsCustomersAPI-resolved-v1-1-4.json` },
+      { name: "Solidarity Groups Customers API", url: `${backendUrl}/CUSTOMERCOBIS/COBISSolidarityGroupsCustomersAPI-resolved-v1-0-1.json` }
     ],    
     Orders:[
-      { name: "Orders API", url: `${process.env.PUBLIC_URL}/ORDERSCOBIS/COBIS Orders API-v1-oas30.json` },
-      { name: "Businesses Customers API", url: `${process.env.PUBLIC_URL}/ORDERSCOBIS/COBIS Orders Template API-v1-oas30.json` }
+      { name: "Orders API", url: `${backendUrl}/ORDERSCOBIS/COBIS Orders API-v1-oas30.json` },
+      { name: "Businesses Customers API", url: `${backendUrl}/ORDERSCOBIS/COBIS Orders Template API-v1-oas30.json` }
     ],
     DemandDepositsAccounts:[
-      { name: "Automatic Savings Plan Demand Deposits", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISAutomaticSavingsPlanDemandDepositsAPI-resolved-v1-0-1.json` },
-      { name: "Balances Demand Deposits API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISBalancesDemandDepositsAPI-resolved-v2-4-7.json` },
-      { name: "Demand Deposits API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISDemandDepositsAPI-resolved-v2-5-10.json` },
-      { name: "Checking Account API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISCheckingAccountAPI-resolved-v1-3-1.json` },
-      { name: "Financial Transactional Demand Deposits API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISFinancialTransactionalDemandDepositsAPI-resolved-v2-4-3.json` },
-      { name: "Freezing Demand Deposits API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISFreezingDemandDepositsAPI-resolved-v2-4-5.json` },
-      { name: "Overdraft Checking Account API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISOverdraftCheckingAccountAPI-resolved-v1-5-6.json` },
-      { name: "Transactional Demand Deposits API", url: `${process.env.PUBLIC_URL}/DEMANDDEPOSITS/COBISTransactionalDemandDepositsAPI-resolved-v2-6-8.json` }
+      { name: "Automatic Savings Plan Demand Deposits", url: `${backendUrl}/DEMANDDEPOSITS/COBISAutomaticSavingsPlanDemandDepositsAPI-resolved-v1-0-1.json` },
+      { name: "Balances Demand Deposits API", url: `${backendUrl}/DEMANDDEPOSITS/COBISBalancesDemandDepositsAPI-resolved-v2-4-7.json` },
+      { name: "Demand Deposits API", url: `${backendUrl}/DEMANDDEPOSITS/COBISDemandDepositsAPI-resolved-v2-5-10.json` },
+      { name: "Checking Account API", url: `${backendUrl}/DEMANDDEPOSITS/COBISCheckingAccountAPI-resolved-v1-3-1.json` },
+      { name: "Financial Transactional Demand Deposits API", url: `${backendUrl}/DEMANDDEPOSITS/COBISFinancialTransactionalDemandDepositsAPI-resolved-v2-4-3.json` },
+      { name: "Freezing Demand Deposits API", url: `${backendUrl}/DEMANDDEPOSITS/COBISFreezingDemandDepositsAPI-resolved-v2-4-5.json` },
+      { name: "Overdraft Checking Account API", url: `${backendUrl}/DEMANDDEPOSITS/COBISOverdraftCheckingAccountAPI-resolved-v1-5-6.json` },
+      { name: "Transactional Demand Deposits API", url: `${backendUrl}/DEMANDDEPOSITS/COBISTransactionalDemandDepositsAPI-resolved-v2-6-8.json` }
     ],
     TimeDeposits:[
-      { name: "TimeDepositsAPI", url: `${process.env.PUBLIC_URL}/TIMEDEPOSITS/COBISTimeDepositsAPI.json` },
-      { name: "TimeDepositsBlocksAPI", url: `${process.env.PUBLIC_URL}/TIMEDEPOSITS/COBISTimeDepositsBlocksAPI.json` },
-      { name: "TimeDepositsManagementAPI", url: `${process.env.PUBLIC_URL}/TIMEDEPOSITS/COBISTimeDepositsManagementAPI.json` }
+      { name: "TimeDepositsAPI", url: `${backendUrl}/TIMEDEPOSITS/COBISTimeDepositsAPI.json` },
+      { name: "TimeDepositsBlocksAPI", url: `${backendUrl}/TIMEDEPOSITS/COBISTimeDepositsBlocksAPI.json` },
+      { name: "TimeDepositsManagementAPI", url: `${backendUrl}/TIMEDEPOSITS/COBISTimeDepositsManagementAPI.json` }
     ],
     Admin:[
-      { name: "AdminAPI", url: `${process.env.PUBLIC_URL}/ADMIN/COBISAdminAPI.json` }
+      { name: "AdminAPI", url: `${backendUrl}/ADMIN/COBISAdminAPI.json` }
     ],
     Authentication:[
-      { name: "COBISSystemAuthorizationAPI", url: `${process.env.PUBLIC_URL}/AUTHENTICATION/COBISSystemAuthorizationAPI-resolved-v1-0-0.json` },
-      { name: "COBISOfficialAuthentication", url: `${process.env.PUBLIC_URL}/AUTHENTICATION/COBISOfficialAuthenticationV2API-resolved-v2-0-1.json` }
+      { name: "COBISSystemAuthorizationAPI", url: `${backendUrl}/AUTHENTICATION/COBISSystemAuthorizationAPI-resolved-v1-0-0.json` },
+      { name: "COBISOfficialAuthentication", url: `${backendUrl}/AUTHENTICATION/COBISOfficialAuthenticationV2API-resolved-v2-0-1.json` }
     ],
     FinancialProductManager:[
-      { name: "FinancialProductManager", url: `${process.env.PUBLIC_URL}/FINANCIALPRODUCTMANAGER/COBISFinancialProductManagerV2API-resolved-v2-0-0.json` }
+      { name: "FinancialProductManager", url: `${backendUrl}/FINANCIALPRODUCTMANAGER/COBISFinancialProductManagerV2API-resolved-v2-0-0.json` }
     ]
   };
 
